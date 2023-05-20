369cad1d6d39cfece8935ea6c1522748326244a0
Filip Drobiazgiewicz <fdrobiazg@192.168.1.7>
2023-05-20 19:28:14 +0200

    processing service update

--- src/processing/main.py
+++ src/processing/main.py
@@ -1,11 +1,39 @@
-import os
+import os, pika
 from utils import clr2gray
+from pymongo import MongoClient
+import gridfs
 
 MONGO_HOST = os.environ.get("MONGO_HOST")
-MONGO_PORT = os.environ.get("MONGO_PORT")
+MONGO_USER = os.environ.get("MONGO_USER")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD")
+
+RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST")
+SVC_QUEUE = os.environ.get("SVC_QUEUE")
 
 def main():
-    pass
+    mongo = MongoClient(MONGO_HOST,
+                     username=MONGO_USER,
+                     password=MONGO_PASSWORD,
+                     authSource='admin')
+    
+    src_gfs = gridfs.GridFS(mongo['src_img'])
+    out_gfs = gridfs.GridFS(mongo['processed_img'])
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+    channel = connection.channel()
+
+    def callback(ch, method, properties, body):
+        err = clr2gray.convert(body, src_gfs, out_gfs, ch)
+        if err:
+            ch.basic_nack(delivery_tag=method.delivery_tag)
+        else:
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_consume(
+        queue=SVC_QUEUE, on_message_callback=callback
+    )
+
+    channel.start_consuming()
 
 if __name__ == "__main__":
     main()
--- src/processing/utils/clr2gray.py
+++ src/processing/utils/clr2gray.py
@@ -1,5 +1,32 @@
+import json
+from bson.objectid import ObjectId
+import numpy as np
 import cv2
+import tempfile
 
-def convert(img): 
-    source = cv2.imread(img)
-    converted = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
+def convert(message, gfs_src, gfs_out, channel): 
+    message = json.load(message)
+    img = read_to_cv2(gfs_src, ObjectId(message['src_fid']))
+    with tempfile.NamedTemporaryFile(suffix=".png") as tf:
+        cv2.imwrite(tf, img)
+
+
+
+def read_to_cv2(gfs_src, objectid):
+    img = gfs_src.get(objectid)
+    img_str = img.read()
+    img_arr = np.fromstring(img_str, np.uint8)
+    img_np = cv2.imdecode(img_arr, cv2.IMREAD_GRAYSCALE)
+    img_cv = cv2.cv.CreateImageHeader((img_np.shape[1], img_np.shape[0]), cv2.cv.IPL_DEPTH_8U, 3)
+    cv2.cv.SetData(img_cv, img_np.tostring(), img_np.dtype.itemsize * 3 * img_np.shape[1])
+
+    return img_cv
+
+def save(gfs_out, file):
+    try:
+        res = gfs_out.put(file)
+    except Exception as err:
+        return f"Failed to save file. Error:\n {err}", 500
+
+
+        

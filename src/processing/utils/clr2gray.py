d7533381956f8235922f01940e5e21f33286f780
Filip Drobiazgiewicz <fdrobiazg@192.168.1.7>
2023-05-20 23:32:32 +0200

    processing adjustments

--- src/processing/utils/clr2gray.py
+++ src/processing/utils/clr2gray.py
@@ -5,28 +5,26 @@ import cv2
 import tempfile
 
 def convert(message, gfs_src, gfs_out, channel): 
-    message = json.load(message)
-    img = read_to_cv2(gfs_src, ObjectId(message['src_fid']))
+    message = json.loads(message)
+    img = read_to_cv2(gfs_src, ObjectId(message['img_fid']))
     with tempfile.NamedTemporaryFile(suffix=".png") as tf:
-        cv2.imwrite(tf, img)
-
-
+        cv2.imwrite(tf.name, img)
+        save(gfs_out, tf)
 
 def read_to_cv2(gfs_src, objectid):
-    img = gfs_src.get(objectid)
-    img_str = img.read()
-    img_arr = np.fromstring(img_str, np.uint8)
+    img = gfs_src.get(objectid).read()
+    img_arr = np.fromstring(img, np.uint8)
     img_np = cv2.imdecode(img_arr, cv2.IMREAD_GRAYSCALE)
-    img_cv = cv2.cv.CreateImageHeader((img_np.shape[1], img_np.shape[0]), cv2.cv.IPL_DEPTH_8U, 3)
-    cv2.cv.SetData(img_cv, img_np.tostring(), img_np.dtype.itemsize * 3 * img_np.shape[1])
+    # img_cv = cv2.CreateImageHeader((img_np.shape[1], img_np.shape[0]), cv2.IPL_DEPTH_8U, 3)
+    # cv2.SetData(img_cv, img_np.tostring(), img_np.dtype.itemsize * 3 * img_np.shape[1])
 
-    return img_cv
+    return img_np
 
 def save(gfs_out, file):
     try:
         res = gfs_out.put(file)
-    except Exception as err:
-        return f"Failed to save file. Error:\n {err}", 500
+    except Exception:
+        return f"Failed to save file.", 500
 
 
         

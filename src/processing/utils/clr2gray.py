fc9eb963754d03eccdbd6d22be1a3ecc05e8b440
Filip Drobiazgiewicz <fdrobiazg@192.168.1.7>
2023-05-21 19:37:23 +0200

    logging and fid printing added

--- src/processing/utils/clr2gray.py
+++ src/processing/utils/clr2gray.py
@@ -4,7 +4,8 @@ import numpy as np
 import cv2
 import tempfile
 
-def convert(message, gfs_src, gfs_out, channel): 
+def convert(message, gfs_src, gfs_out, channel):
+    print(json.dumps({"info": "convert request recived"})) 
     message = json.loads(message)
     img = read_to_cv2(gfs_src, ObjectId(message['img_fid']))
     with tempfile.NamedTemporaryFile(suffix=".png") as tf:
@@ -15,14 +16,14 @@ def read_to_cv2(gfs_src, objectid):
     img = gfs_src.get(objectid).read()
     img_arr = np.fromstring(img, np.uint8)
     img_np = cv2.imdecode(img_arr, cv2.IMREAD_GRAYSCALE)
-    # img_cv = cv2.CreateImageHeader((img_np.shape[1], img_np.shape[0]), cv2.IPL_DEPTH_8U, 3)
-    # cv2.SetData(img_cv, img_np.tostring(), img_np.dtype.itemsize * 3 * img_np.shape[1])
 
     return img_np
 
 def save(gfs_out, file):
     try:
         res = gfs_out.put(file)
+        print(json.dumps({"info": "Convertion finished"}))
+        print(json.dumps({"fid": str(res)}))
     except Exception:
         return f"Failed to save file.", 500
 
